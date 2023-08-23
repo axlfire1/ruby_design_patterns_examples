0c391598dd8f80afdff21716d65740694e7fb244
David Sarmiento <edts83@gmail.com>
2023-08-23 13:16:32 -0600

    first commit

---
+++ coffee.rb
@@ -0,0 +1,17 @@
+require_relative 'beverage'
+
+require 'byebug'
+
+class Coffee
+  def cost
+    puts('the cost 50$')
+  end
+end
+
+c_obj = Coffee.new
+c_obj.cost
+
+c_obj_decorator = Coffee.new
+c_obj_decorator.extend(Beverage)
+c_obj_decorator.cost
+c_obj_decorator.discount
